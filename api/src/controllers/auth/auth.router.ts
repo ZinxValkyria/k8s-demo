051fcd47fffd32f4d721563cafd25c432c964174
Alex Crease <119538960+Apollo-XIV@users.noreply.github.com>
2023-06-20 22:50:54 +0100

    pppp

--- api/src/controllers/auth/auth.router.ts
+++ api/src/controllers/auth/auth.router.ts
@@ -1,3 +1,29 @@
 import express, {Request, Response} from "express";
+import type { NextApiRequest, NextApiResponse } from "next"
+import NextAuth, { AuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { config } from "dotenv";
+import { initialEnv } from "@next/env"
 
+config();
+if (!process.env.GITHUB_ID || !process.env.GITHUB_SECRET) {throw new Error("REQUIRED ENV VARS NOT SET")}
+
+const options: AuthOptions = {
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+    ]
+}
 export const authRouter = express.Router();
+
+authRouter.all("*", (req: any, res: any) => {
+    req.query.nextauth = req.url
+                            .slice(1)
+                            .replace(/\?.*/,"")
+                            .split("/");
+    req.url = '/api/auth'+req.url;
+    console.log(req.query.nextauth)
+    return NextAuth(req, res, options); 
+})
--- api/src/index.ts
+++ api/src/index.ts
@@ -3,8 +3,10 @@ import * as dotenv from "dotenv";
 import express, {Request, Response} from "express";
 import cors from "cors";
 import helmet from "helmet";
+import cookieParser from "cookie-parser";
 import { userRouter } from "./controllers/users/user.router";
 import { chirpRouter } from "./controllers/chirps/chirp.router";
+import { authRouter } from "./controllers/auth/auth.router";
 import { connect } from "./modules/db";
 
 dotenv.config();
@@ -23,16 +25,19 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
 
 
 
 // Routes //====>
 app.use("/user", userRouter);
 app.use("/chirp", chirpRouter);
+app.use("/api/auth", authRouter);
 
 app.get("/", async (req: Request, res: Response) => {
-    console.log("request received");
-    res.status(200).send("tested");
+    console.log("request received... BOUNCE");
+    res.status(200).send("bounce");
 })
 
 // Server Init //====>
